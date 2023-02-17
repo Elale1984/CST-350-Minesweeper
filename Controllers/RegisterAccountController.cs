4fedfb31bcfd535e31090f8db89b5f03859d4780
Edward <edward.gratefuldead1@gmail.com>
2023-02-17 16:30:07 -0600

    Created the Model, View, and Controller for Registration page. With Validation.

---
+++ Controllers/RegisterAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CST_350_Minesweeper.Controllers
+{
+    public class RegisterAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CST_350_Minesweeper.Models
+{
+    public class User
+    {
+        // First Name, Last Name, Sex, Age, State, Email Address, Username, and Password
+
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Sex is required. Please Enter Male or Female")]
+        public string Sex { get; set; }
+
+        [Range(13, 120, ErrorMessage = "Age must be between 13 and 120.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "State is required.")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Email address is required.")]
+        [RegularExpression(@"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$", ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters.")]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\da-zA-Z]).{8,}$",
+                           ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        public string Password { get; set; }
+
+    }
+}
